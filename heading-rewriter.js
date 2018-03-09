04a554020c75b75d48fec751b90aa8112808d8e2
HurrieCrane <the.cranejoe@gmail.com>
2018-03-09 13:23:38 +0000

    Added missng JS stuff

---
+++ heading-rewriter.js
@@ -0,0 +1,23 @@
+// find out what time it is
+
+var currenttime = new Date();
+var currenthour = currenttime.getHours();
+
+// do a conditional
+
+var welcomemessage = "My own personal website";
+
+if(currenthour>5 && currenthour < 12){
+  welcomemessage = "Good morning, welcome to my personal website";  
+}
+
+if(currenthour>12 && currenthour < 18){
+  welcomemessage = "Good afternoon, welcome to my personal website";  
+}
+
+if(currenthour>18 && currenthour < 22){
+  welcomemessage = "Good evening, welcome to my personal website";  
+}
+
+var getheading = document.querySelector('.header-content h1');
+getheading.insertAdjacentHTML('afterend','<p>'+welcomemessage+'</p>');
