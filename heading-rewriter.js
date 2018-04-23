0001c959118426d2df1286bb9fc0cb3a5ecd4bb0
JackPennington <penningtonjack@hotmail.co.uk>
2018-04-23 14:28:42 +0100

    Testing push

--- heading-rewriter.js
+++ heading-rewriter.js
@@ -5,19 +5,29 @@ var currenthour = currenttime.getHours();
 
 // do a conditional
 
-var welcomemessage = "My own personal website";
-
-if(currenthour>5 && currenthour < 12){
-  welcomemessage = "Good morning, welcome to my personal website";  
+function getWelcomeMessage() {
+  var welcomemessage = "My own personal website";
+  
+  if(currenthour>=5 && currenthour < 12){
+    welcomemessage = "Good morning, welcome to my personal website";  
+  }
+  
+  if(currenthour>=12 && currenthour < 18){
+    welcomemessage = "Good afternoon, welcome to my personal website";  
+  }
+  
+  if(currenthour>=18 && currenthour < 22){
+    welcomemessage = "Good evening, welcome to my personal website";  
+  }
+  
+  return welcomemessage
 }
 
-if(currenthour>12 && currenthour < 18){
-  welcomemessage = "Good afternoon, welcome to my personal website";  
+function insertWelcomeMessage() {
+  var getheading = document.querySelector('.header-content h1');
+  getheading.insertAdjacentHTML('afterend','<p class="mainheading welcomemessage">'+ getWelcomeMessage() +'</p>');
 }
 
-if(currenthour>18 && currenthour < 22){
-  welcomemessage = "Good evening, welcome to my personal website";  
-}
+insertWelcomeMessage();
 
-var getheading = document.querySelector('.header-content h1');
-getheading.insertAdjacentHTML('afterend','<p>'+welcomemessage+'</p>');
+document.querySelector('.header-bg').classList.add('header-bg--show');
